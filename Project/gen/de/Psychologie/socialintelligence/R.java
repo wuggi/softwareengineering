4909635ccf9259759cd3ebc1d42b9bd556018274
Jens Wiemann <jens.wiemann@hotmail.com>
2013-04-27 15:48:07 +0200

    Neu

--- Project/gen/de/Psychologie/socialintelligence/R.java
+++ Project/gen/de/Psychologie/socialintelligence/R.java
@@ -35,8 +35,8 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Anleitung=0x7f050002;
         public static final int action_settings=0x7f050001;
-        public static final int anleitung=0x7f050002;
         public static final int app_name=0x7f050000;
     }
     public static final class style {
