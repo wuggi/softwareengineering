69117fd2bafed49fb6a705688cbb4635fd22df07
csteusloff <steusloff@csLinux.(none)>
2013-04-27 17:59:03 +0200

    for Jens

--- Project/src/de/Psychologie/socialintelligence/MainActivity.java
+++ Project/src/de/Psychologie/socialintelligence/MainActivity.java
@@ -3,14 +3,31 @@ package de.Psychologie.socialintelligence;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	Button btnWeiter;
+	EditText userCode;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnWeiter = (Button)findViewById(R.id.btnWeiter);
+		btnWeiter.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				userCode = (EditText)findViewById(R.id.userCode);
+				String code = userCode.getText().toString();
+				SQLHandler db = new SQLHandler(MainActivity.this);
+				
+			}
+		});
 	}
 
 	@Override
