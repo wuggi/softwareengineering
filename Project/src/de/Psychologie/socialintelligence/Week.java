f9c83479d76cb825367f431b87044e84c70b29ed
wuggi <patrick.wuggazer@st.ovgu.de>
2013-04-27 16:24:24 +0200

    neu

--- Project/src/de/Psychologie/socialintelligence/Week.java
+++ Project/src/de/Psychologie/socialintelligence/Week.java
@@ -10,6 +10,9 @@ public class Week extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_week);
+		
+		
+		
 	}
 
 	@Override
