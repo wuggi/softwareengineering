526b74cef1995ef87f4d9ca4479f63f0e2f2a3a9
csteusloff <steusloff@csLinux.(none)>
2013-04-27 17:41:42 +0200

    test

--- Project/gen/de/Psychologie/socialintelligence/R.java
+++ Project/gen/de/Psychologie/socialintelligence/R.java
@@ -10,6 +10,10 @@ package de.Psychologie.socialintelligence;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int checkButton=0x7f040000;
+        public static final int uncheckedButton=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,39 +21,36 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout2=0x7f080005;
-        public static final int action_settings=0x7f080019;
-        public static final int button1=0x7f080002;
-        public static final int editText1=0x7f080000;
-        public static final int tableRow1=0x7f080006;
-        public static final int tableRow2=0x7f08000b;
-        public static final int tableRow3=0x7f08000f;
-        public static final int tableRow4=0x7f080014;
-        public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080001;
-        public static final int textView3=0x7f080003;
-        public static final int timeslot1=0x7f080007;
-        public static final int timeslot10=0x7f080012;
-        public static final int timeslot11=0x7f080013;
-        public static final int timeslot12=0x7f080015;
-        public static final int timeslot13=0x7f080016;
-        public static final int timeslot14=0x7f080017;
-        public static final int timeslot15=0x7f080018;
-        public static final int timeslot2=0x7f080008;
-        public static final int timeslot3=0x7f080009;
-        public static final int timeslot4=0x7f08000a;
-        public static final int timeslot5=0x7f08000c;
-        public static final int timeslot6=0x7f08000d;
-        public static final int timeslot7=0x7f08000e;
-        public static final int timeslot8=0x7f080010;
-        public static final int timeslot9=0x7f080011;
+        public static final int LinearLayout2=0x7f090002;
+        public static final int action_settings=0x7f090016;
+        public static final int editText1=0x7f090001;
+        public static final int tableRow1=0x7f090003;
+        public static final int tableRow2=0x7f090008;
+        public static final int tableRow3=0x7f09000c;
+        public static final int tableRow4=0x7f090011;
+        public static final int textView1=0x7f090000;
+        public static final int timeslot1=0x7f090004;
+        public static final int timeslot10=0x7f09000f;
+        public static final int timeslot11=0x7f090010;
+        public static final int timeslot12=0x7f090012;
+        public static final int timeslot13=0x7f090013;
+        public static final int timeslot14=0x7f090014;
+        public static final int timeslot15=0x7f090015;
+        public static final int timeslot2=0x7f090005;
+        public static final int timeslot3=0x7f090006;
+        public static final int timeslot4=0x7f090007;
+        public static final int timeslot5=0x7f090009;
+        public static final int timeslot6=0x7f09000a;
+        public static final int timeslot7=0x7f09000b;
+        public static final int timeslot8=0x7f09000d;
+        public static final int timeslot9=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -57,20 +58,18 @@ public final class R {
         public static final int activity_week=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int pop_poll=0x7f070001;
-        public static final int week=0x7f070002;
+        public static final int main=0x7f080000;
+        public static final int pop_poll=0x7f080001;
+        public static final int week=0x7f080002;
     }
     public static final class string {
-        public static final int Anleitung=0x7f050006;
-        public static final int Willkommen=0x7f050007;
-        public static final int Willkommen_2=0x7f050008;
-        public static final int action_settings=0x7f050005;
-        public static final int anleitung=0x7f050000;
-        public static final int app_name=0x7f050004;
-        public static final int time_slot=0x7f050002;
-        public static final int title_activity_pop_poll=0x7f050003;
-        public static final int title_activity_week=0x7f050001;
+        public static final int Anleitung=0x7f060006;
+        public static final int action_settings=0x7f060001;
+        public static final int anleitung=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int time_slot=0x7f060005;
+        public static final int title_activity_pop_poll=0x7f060003;
+        public static final int title_activity_week=0x7f060004;
     }
     public static final class style {
         /** 
@@ -94,10 +93,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
