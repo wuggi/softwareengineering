b6531349c44de77376a8e09e1840b4586eeb261a
Jens Wiemann <jens.wiemann@hotmail.com>
2013-05-11 20:25:10 +0200

    Klingelton in der Summary
    - Der Klingelton steht nun in der Summary nach auswahl

--- Project/src/de/Psychologie/socialintelligence/UserSettingActivity.java
+++ Project/src/de/Psychologie/socialintelligence/UserSettingActivity.java
@@ -3,11 +3,15 @@ package de.Psychologie.socialintelligence;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
+import android.preference.RingtonePreference;
 
 public class UserSettingActivity extends PreferenceActivity {
 
@@ -44,28 +48,55 @@ public class UserSettingActivity extends PreferenceActivity {
 				.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
 					@Override
 					public boolean onPreferenceClick(Preference arg0) {
-						AlertDialog ad = new AlertDialog.Builder(UserSettingActivity.this).create();  
-						ad.setCancelable(false); // This blocks the 'BACK' button  
-						ad.setMessage(getResources().getString(R.string.message_about));
-						ad.setButton("OK", new DialogInterface.OnClickListener() {  
-						    @Override  
-						    public void onClick(DialogInterface dialog, int which) {  
-						        dialog.dismiss();                      
-						    }  
-						});  
-						ad.show(); 
+						AlertDialog ad = new AlertDialog.Builder(
+								UserSettingActivity.this).create();
+						ad.setCancelable(false); // This blocks the 'BACK'
+													// button
+						ad.setMessage(getResources().getString(
+								R.string.message_about));
+						ad.setButton("OK",
+								new DialogInterface.OnClickListener() {
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which) {
+										dialog.dismiss();
+									}
+								});
+						ad.show();
 						return true;
 					}
 				});
-		ListPreference myPreference = (ListPreference) findPreference("Sleeptime");
-		myPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-		    @Override
-		    public boolean onPreferenceChange(Preference preference, Object newValue) {
-		            preference.setSummary(((String)newValue)+"\tMinuten");		        
-				return true;
-		    }
-		});
+
+		RingtonePreference ringtonepref = (RingtonePreference) findPreference("ringtone");
+		ringtonepref
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						Uri ringtoneUri = Uri.parse((String) newValue);
+						Ringtone ringtone = RingtoneManager.getRingtone(
+								UserSettingActivity.this, ringtoneUri);
+						String name = ringtone.getTitle(UserSettingActivity.this);
+
+						preference.setSummary( name);
+
+						return true;
+					}
+				});
+
+		ListPreference sleeptimechooser = (ListPreference) findPreference("Sleeptime");
+		sleeptimechooser
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						preference
+								.setSummary(((String) newValue) + "\tMinuten");
+						return true;
+					}
+				});
 	}
+
 	@Override
 	public void onBackPressed() {
 		// Wenn es nicht der knoten ist, soll es geschlossen werden
@@ -74,6 +105,5 @@ public class UserSettingActivity extends PreferenceActivity {
 		else
 			super.onBackPressed();
 	}
-	
-	
+
 }
