2187cd683b36b873be0b71c54d97a00d85764f01
Jens Wiemann <jens.wiemann@hotmail.com>
2013-05-14 12:43:08 +0200

    password_text_fix

--- Project/src/de/Psychologie/socialintelligence/AdminSettingsActivity.java
+++ Project/src/de/Psychologie/socialintelligence/AdminSettingsActivity.java
@@ -1,5 +1,8 @@
 package de.Psychologie.socialintelligence;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -7,6 +10,7 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.text.InputType;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -29,22 +33,61 @@ public class AdminSettingsActivity extends PreferenceActivity {
 						db.deleteDB();
 						return true;
 					}
-				});
-		Preference button_password_change = (Preference) findPreference("password");
+				});		
 		
+		
+		Preference button_password_change = (Preference) findPreference("password");
 		button_password_change
-		.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			@Override
-			public boolean onPreferenceChange(Preference preference,
-					Object newValue) {			
-				// Salt and hash the imput and write it manuelly to the preferences
-				final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(AdminSettingsActivity.this);
-				SharedPreferences.Editor editor = settings.edit();
-				editor.putString("password", UserSettingActivity.MD5(((String) newValue) + getResources().getString(R.string.salt)));
-				editor.commit();
-				//Dont change it to newValue
-				return false;
-			}
-		});
+				.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(final Preference arg0) {
+						
+						AlertDialog.Builder builder = new AlertDialog.Builder(AdminSettingsActivity.this);
+						builder.setTitle(getResources().getString(R.string.title_password_entry));
+						final EditText input = new EditText(AdminSettingsActivity.this);
+						//final EditText input2 = new EditText(AdminSettingsActivity.this);
+						// input2.setInputType(InputType.TYPE_CLASS_TEXT |
+						// InputType.TYPE_TEXT_VARIATION_PASSWORD);
+						/*
+						  Context context = mapView.getContext(); LinearLayout
+						  layout = new LinearLayout(context);
+						  layout.setOrientation(LinearLayout.VERTICAL);
+						  
+						  final EditText titleBox = new EditText(context);
+						  titleBox.setHint("Title"); layout.addView(titleBox);
+						  
+						  final EditText descriptionBox = new
+						  EditText(context);
+						  descriptionBox.setHint("Description");
+						  layout.addView(descriptionBox);
+						  
+						  dialog.setView(layout);
+						 */
+						
+						input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+						builder.setView(input);
+						
+				        builder.setCancelable(false)
+				               .setPositiveButton(getResources().getString(R.string.OK), new DialogInterface.OnClickListener() {
+				                   public void onClick(DialogInterface dialog, int id) {
+				                	// Salt and hash the imput and write it manuelly to the preferences
+				       				final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(AdminSettingsActivity.this);
+				       				SharedPreferences.Editor editor = settings.edit();
+				       				editor.putString("password", UserSettingActivity.MD5((input.getText().toString()) + getResources().getString(R.string.salt)));
+				       				editor.commit();				                	
+				                   }
+				               })
+				               .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+				                   public void onClick(DialogInterface dialog, int id) {				                	   
+				                	   dialog.cancel();
+				                   }
+				               });
+				        AlertDialog alert = builder.create();
+				        alert.show();
+						return true;
+					}
+				});
+		
+		
 	}
 };
